30f92b1a7ea1ff74c5d075c2ef99ae8cdf1afb1b
latiefqohar <latiefqohar97@gmail.com>
2019-04-29 20:56:27 +0700

    modif controller mobil

---
+++ application/controllers/mobil.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Mobil extends CI_Controller
+{
+    public function warna()
+    {
+        echo "Mobil itu berwarna". $this->uri->segment('3');
+    }
+}
